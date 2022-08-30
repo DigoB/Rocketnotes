b474e2f2eb3a2e810ab9f59321eda70032dcf8e5
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-30 18:16:16 -0300

    Section component created

--- rocketnotes/src/pages/details/index.jsx
+++ rocketnotes/src/pages/details/index.jsx
@@ -1,12 +1,21 @@
-import { Container } from './styles.js'
+import { Container, Links } from './styles.js'
 import { Header } from '../../components/header'
 import { Button } from '../../components/button'
+import { Section } from '../../components/section'
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+
+      <Section title="Links Úteis">
+        <Links>
+          <li><a href="#">Link 1</a></li>
+        </Links>
+      </Section>
+
+
       <Button title="Voltar"/>
     </Container>
   )
