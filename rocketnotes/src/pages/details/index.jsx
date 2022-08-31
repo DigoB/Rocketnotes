5b7597b1d10e477e37f71bf96c9ce5d86e0cbc1f
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-30 21:51:07 -0300

    Details page finished, starting do configure home page

--- rocketnotes/src/pages/details/index.jsx
+++ rocketnotes/src/pages/details/index.jsx
@@ -1,8 +1,9 @@
-import { Container, Links } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 import { Header } from '../../components/header'
 import { Button } from '../../components/button'
 import { Section } from '../../components/section'
 import { Tag } from '../../components/tag'
+import { ButtonText } from '../../components/buttonText'
 
 export function Details() {
 
@@ -10,18 +11,32 @@ export function Details() {
     <Container>
       <Header />
 
-      <Section title="Links Úteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
 
-      <Section title="Marcadores">
-        <Tag title="Express" />
-        <Tag title="NodeJs" />
-      </Section>
+          <ButtonText title="Excluir nota" />
 
-      <Button title="Voltar"/>
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eius corrupti saepe, porro unde velit officiis molestias perferendis accusamus repellat, voluptas iste quis nisi deserunt eaque, ad vel vero nulla sunt reiciendis autem error aut! Voluptatibus est quod voluptatem odit aut, eos velit rem iste dolore harum maiores reiciendis iusto in repellendus dolores illo consectetur. Aspernatur, voluptas asperiores harum beatae animi illum. Atque autem aperiam vitae cumque maiores accusamus repellendus itaque facere! Reiciendis voluptates mollitia obcaecati impedit unde? Odio omnis quibusdam cumque ipsa delectus suscipit error nisi iste, inventore vel numquam voluptate amet consequatur nihil quas dolorum excepturi. Ab, soluta!</p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li><a href="https://www.github.com/DigoB" target="_blank">https://github.com/DigoB</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="NodeJs" />
+          </Section>
+
+          <Button title="Voltar"/>
+
+        </Content>
+      </main>
     </Container>
   )
   
