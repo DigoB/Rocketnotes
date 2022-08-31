4ad16b7aba167c2c6de7da21f50fb9d8ddf8e020
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-30 22:49:44 -0300

    Components for notes, inputs and home finished

--- rocketnotes/src/pages/home/index.jsx
+++ rocketnotes/src/pages/home/index.jsx
@@ -1,6 +1,10 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from '../../components/header'
 import { ButtonText } from '../../components/buttonText'
+import { Input } from "../../components/input";
+import { Section } from "../../components/section";
+import { Note } from "../../components/note";
+import { FiPlus, FiSearch } from 'react-icons/fi'
 
 export function Home() {
 
@@ -19,14 +23,61 @@ export function Home() {
             </Menu>
 
             <Search>
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
 
             </Search>
 
             <Content>
+                <Section title="Minhas Notas">
+
+                    <Note data={{
+                        title: 'React',
+                         tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'nodejs'}
+                        ]
+                        }} 
+                    />
+                    <Note data={{
+                        title: 'Javascript',
+                         tags: [
+                            {id: '3', name: 'javascript'},
+                            {id: '4', name: 'html'}
+                        ]
+                        }} 
+                    />
+                    <Note data={{
+                        title: 'Nodejs',
+                         tags: [
+                            {id: '5', name: 'npm'},
+                            {id: '6', name: 'express'}
+                        ]
+                        }} 
+                    />
+                    <Note data={{
+                        title: 'Web Develop',
+                         tags: [
+                            {id: '7', name: 'spa'},
+                            {id: '8', name: 'mobile-first'}
+                        ]
+                        }} 
+                    />
+                    <Note data={{
+                        title: 'Front',
+                         tags: [
+                            {id: '1', name: 'html5'},
+                            {id: '2', name: 'css3'}
+                        ]
+                        }} 
+                    />
+
+                </Section>
 
             </Content>
 
             <NewNote>
+                Criar Nota
+                <FiPlus />
 
             </NewNote>
 
