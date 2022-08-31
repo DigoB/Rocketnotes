e8c54f1980e9347530ca3606baa1007d84c5ce9a
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-30 23:48:13 -0300

    Profile page created

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from "./styles/theme"
 import GlobalStyles from './styles/global'
-import { SignUp } from './pages/signUp'
+import { Profile } from './pages/profile'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
